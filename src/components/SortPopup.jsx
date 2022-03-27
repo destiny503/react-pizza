6f585a123cc5a884f82d48cbca9a2713d43d7421
tsartsartsar <a.rahmatullin1999@gmail.com>
2022-03-28 00:08:48 +0400

    sort fix

--- src/components/SortPopup.jsx
+++ src/components/SortPopup.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const SortPopup = React.memo(function SortPopup({items, activeSortType, onClickSortType}) {
+const SortPopup = React.memo(function SortPopup({ items, activeSortType, onClickSortType }) {
 
     const [visiblePopup, setVisiblePopup] = React.useState(false)
     const sortRef = React.useRef()
@@ -34,11 +34,11 @@ const SortPopup = React.memo(function SortPopup({items, activeSortType, onClickS
         <div ref={sortRef} className="sort">
             <div className="sort__label">
                 <svg className={visiblePopup ? 'rotated' : ''}
-                     width="10"
-                     height="6"
-                     viewBox="0 0 10 6"
-                     fill="none"
-                     xmlns="http://www.w3.org/2000/svg"
+                    width="10"
+                    height="6"
+                    viewBox="0 0 10 6"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
                 >
                     <path
                         d="M10 5C10 5.16927 9.93815 5.31576 9.81445 5.43945C9.69075 5.56315 9.54427 5.625 9.375 5.625H0.625C0.455729 5.625 0.309245 5.56315 0.185547 5.43945C0.061849 5.31576 0 5.16927 0 5C0 4.83073 0.061849 4.68424 0.185547 4.56055L4.56055 0.185547C4.68424 0.061849 4.83073 0 5 0C5.16927 0 5.31576 0.061849 5.43945 0.185547L9.81445 4.56055C9.93815 4.68424 10 4.83073 10 5Z"
@@ -51,11 +51,12 @@ const SortPopup = React.memo(function SortPopup({items, activeSortType, onClickS
             {visiblePopup && <div className="sort__popup">
                 <ul>
                     {items && items.map((obj, index) =>
-                        <li className={activeSortType === index ? 'active' : ''}
-                            onClick={() => onSelectItem(index)}
-                            key={`${obj.type}_${index}`}>
-                            {obj.name}
-                        </li>)}
+                    (<li
+                        className={activeSortType === obj.type ? 'active' : ''}
+                        onClick={() => onSelectItem(obj)}
+                        key={`${obj.type}_${index}`}>
+                        {obj.name}
+                    </li>))}
                 </ul>
             </div>}
         </div>
